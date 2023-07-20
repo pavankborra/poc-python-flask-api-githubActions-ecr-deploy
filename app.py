da2924c392bb0d2d30da6a6c5f4182419fb016dd
pavankborra <71217912+pavankborra@users.noreply.github.com>
2023-07-20 22:57:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def post_days():
 if __name__ == "__main__":
     # app.run(debug=True) 
     # Commented the above line and inserted the below
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=3000)
